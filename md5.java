c0cf7db07e50d0fcaf900e8f5f0685ac8200a2ed
Wilson <wilsonact@users.noreply.github.com>
2016-09-22 14:13:55 +0800

    Update md5.java

--- md5.java
+++ md5.java
@@ -31,7 +31,7 @@ public  class md5 {
 		for (int i = 0; i < md5Bytes.length; i++){
 			int val = ((int) md5Bytes[i]) & 0xff;
 			if (val < 16)
-			hexValue.append("0");
+				hexValue.append("0");
 			hexValue.append(Integer.toHexString(val));
 		}
 		return hexValue.toString();
